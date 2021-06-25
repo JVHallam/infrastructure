33a1c479a43aaf37ff1438dec5023b7b88e8916a
Jake Hallam <jakehallam95@gmail.com>
2021-06-25 11:12:43 +0100

    updated keyvault

--- keyvault/main.tf
+++ keyvault/main.tf
@@ -1,3 +1,7 @@
+//Keyvault:
+//az keyvault list
+//az keyvault secret show --vault-name kv-jvh-knxg --name test
+
 provider "azurerm" {
   features {
     key_vault {
@@ -6,34 +10,41 @@ provider "azurerm" {
   }
 }
 
-data "azurerm_client_config" "current" {}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
+module "naming" {
+  source = "github.com/azure/terraform-azurerm-naming"
+  suffix = ["jvh"]
 }
 
-resource "azurerm_key_vault" "example" {
-  name                        = "examplekeyvault"
-  location                    = azurerm_resource_group.example.location
-  resource_group_name         = azurerm_resource_group.example.name
-  enabled_for_disk_encryption = true
-  tenant_id                   = data.azurerm_client_config.current.tenant_id
-  soft_delete_retention_days  = 7
-  purge_protection_enabled    = false
+data "azurerm_client_config" "current" {}
 
-  sku_name = "standard"
+resource "azurerm_resource_group" "rg" {
+  name     = module.naming.resource_group.name_unique
+  location = "UK South"
+}
+
+resource "azurerm_key_vault" "kv" {
+  name                = module.naming.key_vault.name_unique
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku_name            = "standard"
+  tenant_id           = data.azurerm_client_config.current.tenant_id
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
+    //This allows you to set and get keys, secrets and storage
     key_permissions = [
-      "Get",
+      "Get"
     ]
 
     secret_permissions = [
+//MEans you can get it, but doesn't mean you can list them
       "Get",
+//Means create, fucking
+        "Set",
+    //Means you can see them all in the portal
+"List"
     ]
 
     storage_permissions = [
