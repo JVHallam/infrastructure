746c76fdf0210de13fef0764daf46478cb1b2f5c
jake <jakehallam95@gmail.com>
2021-06-28 18:36:26 +0100

    service bus added

---
+++ service_bus/main.tf
@@ -0,0 +1,32 @@
+provider "azurerm" {
+    features {}
+}
+
+module "naming"{
+    source = "github.com/azure/terraform-azurerm-naming"
+    prefix = [ "jvh" ]
+}
+
+locals{
+    loc = "UK South"
+}
+
+resource "azurerm_resource_group" "queue" {
+  name     = module.naming.resource_group.name_unique
+  location = local.loc
+}
+
+//wtf is this?
+resource "azurerm_servicebus_namespace" "queue" {
+  name                = module.naming.servicebus_namespace.name_unique
+  location            = local.loc
+  resource_group_name = azurerm_resource_group.queue.name
+  sku                 = "Standard"
+}
+
+resource "azurerm_servicebus_queue" "queue" {
+  name                = module.naming.servicebus_queue.name_unique
+  resource_group_name = azurerm_resource_group.queue.name
+  namespace_name      = azurerm_servicebus_namespace.queue.name
+  enable_partitioning = true
+}
