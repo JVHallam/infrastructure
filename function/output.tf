a7290e33b2bc5e6b4526af4930429b5db983f9f6
jake <jakehallam95@gmail.com>
2021-06-30 16:04:03 +0100

    New outputs

--- function/output.tf
+++ function/output.tf
@@ -1,3 +1,11 @@
 output "function_app_name"{
     value = azurerm_function_app.fn.name
 }
+
+output "function_app"{
+    value = azurerm_function_app.fn
+}
+
+output "resource_group"{
+    value = azurerm_resource_group.rg
+}
